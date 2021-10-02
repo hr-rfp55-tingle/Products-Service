21253d8ca30a81e8c9c679a700d0d60d6bade964
Justin Beere <beere.justin@gmail.com>
2021-10-02 08:14:27 -0700

    Product Service routes ready for testing and optimization

--- server/db/index.js
+++ server/db/index.js
@@ -1,24 +1,16 @@
 const { Pool } = require('pg');
-const connectionString = 'postgresql://postgres:@localhost:5432/devbox';
+const { connectionString } = require('../../config');
 
 const pool = new Pool({
   connectionString,
 });
 
 module.exports = {
-  // query: (text, params, callback) => {
-  //   const start = Date.now()
-  //   return pool.query(text, params, (err, res) => {
-  //     const duration = Date.now() - start
-  //     console.log('executed query', { text, duration, rows: res.rowCount })
-  //     callback(err, res)
-  //   })
-  // },
   async query(text, params) {
-    const start = Date.now()
-    const res = await pool.query(text, params)
-    const duration = Date.now() - start
-    console.log('executed query', { text, duration, rows: res.rowCount })
-    return res
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log('executed query', { text, duration, rows: res.rowCount });
+    return res;
   },
 }
