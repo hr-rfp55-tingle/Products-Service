3ae90e6bcd59b186ac046e8413e98147ecd9afcd
Justin Beere <beere.justin@gmail.com>
2021-10-04 09:01:29 -0700

    k6 tests for api endpoints

---
+++ k6/related-route.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  const id = Math.floor(Math.random() * 1000000);
+  http.get(`http://localhost:6879/products/${id}/related`);
+  sleep(1);
+}
---
+++ k6/styles-route.js
@@ -0,0 +1,9 @@
+
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  const id = Math.floor(Math.random() * 1000000);
+  http.get(`http://localhost:6879/products/${id}/styles`);
+  sleep(1);
+}
